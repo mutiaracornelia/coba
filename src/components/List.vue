202ecd4b59172ea9e7340545a0e68f8a125c4b68
mutiara cornelia <mutiaracornelia1601@gmail.com>
2023-03-02 16:27:36 +0700

    first commit

---
+++ src/components/List.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+        <ul class="list-group mt-3">
+          <li class="list-group-item" v-for="(todo, index) in todos" :key="index">
+              <div class="row">
+                  <div class="col">
+                      <span v-if="todo.isDone">
+                          <del>{{ todo.activity }}</del>
+                      </span>
+                      <span v-else>{{ todo.activity }}</span>
+                  </div>
+                  <div class="col-auto">
+                      <div class="row gx-2">
+                          <div class="col">
+                              <button @click="deleteTODO(index)" class="btn btn-danger">Delete</button>
+                          </div>
+                          <div class="col">
+                              <button @click="done(index)" class="btn btn-primary">Done</button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </li>
+        </ul>
+      </div>
+</template>
+
+<script>
+export default{
+  props: {
+      todos:{
+          type: Array,
+          default: []
+      }
+  },
+  methods: {
+      deleteTODO(index) {
+         this.$emit('deleteTodo', index); 
+      },
+      done(index){
+      this.$emit('doneTodo', index)
+      }
+  }
+}
+</script>
